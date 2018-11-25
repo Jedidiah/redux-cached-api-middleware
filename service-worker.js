d30915e2a39c2e1e124f45e9c8e75b00303d5290
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-25 17:15:44 +0000

    Deploy buz-zard/redux-cached-api-middleware to github.com/buz-zard/redux-cached-api-middleware.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/redux-cached-api-middleware/demo.89e5b693.css","582a01964ca69a032c2c130bc965bb0f"],["/redux-cached-api-middleware/demo.ab7bf717.js","2c7b2bba36f47b6c570c9236f2c24ec8"],["/redux-cached-api-middleware/index.html","03e42b76772e2c8c3647fd4e72d48dd2"]];
+var precacheConfig = [["/redux-cached-api-middleware/demo.376ccfaa.js","81a7f1cf08275118a7594249c216275e"],["/redux-cached-api-middleware/demo.96c33c9f.css","582a01964ca69a032c2c130bc965bb0f"],["/redux-cached-api-middleware/index.html","f85dec4778b45fba1058e0c488587447"]];
 var cacheName = 'sw-precache-v3-redux-cached-api-middleware-' + (self.registration ? self.registration.scope : '');
 
 
@@ -45,7 +45,7 @@ var ignoreUrlParametersMatching = [/^utm_/];
 
 
 
-var addDirectoryIndex = function(originalUrl, index) {
+var addDirectoryIndex = function (originalUrl, index) {
     var url = new URL(originalUrl);
     if (url.pathname.slice(-1) === '/') {
       url.pathname += index;
@@ -53,7 +53,7 @@ var addDirectoryIndex = function(originalUrl, index) {
     return url.toString();
   };
 
-var cleanResponse = function(originalResponse) {
+var cleanResponse = function (originalResponse) {
     // If this is not a redirected response, then we don't have to do anything.
     if (!originalResponse.redirected) {
       return Promise.resolve(originalResponse);
@@ -75,7 +75,7 @@ var cleanResponse = function(originalResponse) {
     });
   };
 
-var createCacheKey = function(originalUrl, paramName, paramValue,
+var createCacheKey = function (originalUrl, paramName, paramValue,
                            dontCacheBustUrlsMatching) {
     // Create a new URL object to avoid modifying originalUrl.
     var url = new URL(originalUrl);
@@ -91,7 +91,7 @@ var createCacheKey = function(originalUrl, paramName, paramValue,
     return url.toString();
   };
 
-var isPathWhitelisted = function(whitelist, absoluteUrlString) {
+var isPathWhitelisted = function (whitelist, absoluteUrlString) {
     // If the whitelist is empty, then consider all URLs to be whitelisted.
     if (whitelist.length === 0) {
       return true;
@@ -104,7 +104,7 @@ var isPathWhitelisted = function(whitelist, absoluteUrlString) {
     });
   };
 
-var stripIgnoredUrlParameters = function(originalUrl,
+var stripIgnoredUrlParameters = function (originalUrl,
     ignoreUrlParametersMatching) {
     var url = new URL(originalUrl);
     // Remove the hash; see https://github.com/GoogleChrome/sw-precache/issues/290
